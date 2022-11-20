68ea78107711711c3d0428843f2f95e886dce531
Kordian Bruck <k@bruck.me>
2022-11-20 16:20:03 +0100

    Upgrade to newest library versions.

--- src/index.php
+++ src/index.php
@@ -1,7 +1,9 @@
 <?php
 
-use Eluceo\iCal\Component\Calendar;
+use Eluceo\iCal\Domain\Entity\Calendar;
+use Eluceo\iCal\Domain\Entity\TimeZone;
 use ICal\ICal;
+use DateTimeZone as PhpDateTimeZone;
 
 //Global absolute path
 $appPath = realpath(dirname(__FILE__));
@@ -12,13 +14,15 @@ if (!preg_match('!/$!', $appPath)) {
 //Store in constants
 define('APPLICATION_PATH', $appPath);
 define('TIMEZONE', 'Europe/Berlin');
+ini_set('memory_limit', 8589934592);
 
 //Setup Timezone
 $defaultTimeZone = new \DateTimeZone(TIMEZONE);
 date_default_timezone_set(TIMEZONE);
 
 //Include composer components
-require $appPath . './vendor/autoload.php';
+require $appPath . '../vendor/autoload.php';
+require $appPath . 'handler.php';
 
 //Don't output errors
 error_reporting(E_ALL);
@@ -32,10 +36,15 @@ mb_internal_encoding('UTF-8');
  * Show a nice information overview page, if the parameters are not set
  * Also catch ppl trying to inject something over the parameters.
  */
-if (!isset($_GET['pStud'], $_GET['pToken']) || !ctype_alnum($_GET['pStud']) || !ctype_alnum($_GET['pToken'])) {
+if (!isset($_GET['pStud'], $_GET['pToken']) ) {
     header('Content-Type: text/html; charset=UTF-8');
     include APPLICATION_PATH . 'about.php';
     die();
+}
+
+// Catch people trying funny stuff
+if(!ctype_alnum($_GET['pStud']) || !ctype_alnum($_GET['pToken'])){
+    die('Ungültige ID angegeben!');
 }
 
 /**
@@ -47,50 +56,26 @@ $allEvents = $iCal->events();
 
 //Check if anything was received
 if (empty($allEvents)) {
-    die('Die parameter sind ung&uuml;ltig oder ein anderer Fehler ist aufgetreten');
+    die('Die parameter sind ungültig oder ein anderer Fehler ist aufgetreten. Es konnten keine Termine gefunden werden.');
 }
 
 //Remove dupes
 CalProxy\handler::noDupes($allEvents);
 
-//Create new object for outputting the new calender
-$cal = new Calendar('TUM iCal Proxy');
-
-// Create timezone rule object for Daylight Saving Time
-$vTimezoneRuleDst = new \Eluceo\iCal\Component\TimezoneRule(\Eluceo\iCal\Component\TimezoneRule::TYPE_DAYLIGHT);
-$vTimezoneRuleDst->setTzName('CEST');
-$vTimezoneRuleDst->setDtStart(new \DateTime('1981-03-29 02:00:00', $defaultTimeZone));
-$vTimezoneRuleDst->setTzOffsetFrom('+0100');
-$vTimezoneRuleDst->setTzOffsetTo('+0200');
-$dstRecurrenceRule = new \Eluceo\iCal\Property\Event\RecurrenceRule();
-$dstRecurrenceRule->setFreq(\Eluceo\iCal\Property\Event\RecurrenceRule::FREQ_YEARLY);
-$dstRecurrenceRule->setByMonth(3);
-$dstRecurrenceRule->setByDay('-1SU');
-$vTimezoneRuleDst->setRecurrenceRule($dstRecurrenceRule);
-// Create timezone rule object for Standard Time
-$vTimezoneRuleStd = new \Eluceo\iCal\Component\TimezoneRule(\Eluceo\iCal\Component\TimezoneRule::TYPE_STANDARD);
-$vTimezoneRuleStd->setTzName('CET');
-$vTimezoneRuleStd->setDtStart(new \DateTime('1996-10-27 03:00:00', $defaultTimeZone));
-$vTimezoneRuleStd->setTzOffsetFrom('+0200');
-$vTimezoneRuleStd->setTzOffsetTo('+0100');
-$stdRecurrenceRule = new \Eluceo\iCal\Property\Event\RecurrenceRule();
-$stdRecurrenceRule->setFreq(\Eluceo\iCal\Property\Event\RecurrenceRule::FREQ_YEARLY);
-$stdRecurrenceRule->setByMonth(10);
-$stdRecurrenceRule->setByDay('-1SU');
-$vTimezoneRuleStd->setRecurrenceRule($stdRecurrenceRule);
-// Create timezone definition and add rules
-$vTimezone = new \Eluceo\iCal\Component\Timezone(TIMEZONE);
-$vTimezone->addComponent($vTimezoneRuleDst);
-$vTimezone->addComponent($vTimezoneRuleStd);
-$cal->setTimezone($vTimezone);
-
+// Do the transformation
 $handler =  new CalProxy\handler();
-//Event loop
+$newEvents = [];
 foreach ($allEvents as $e) {
-    $cal->addComponent($handler->cleanEvent($e));
+    $newEvents[] = $handler->cleanEvent($e);
 }
 
+//Create new object for outputting the new calendar
+$calendar = new Calendar($newEvents);
+
+// 3. Transform domain entity into an iCalendar component
+$componentFactory = new Eluceo\iCal\Presentation\Factory\CalendarFactory();
+$calendarComponent = $componentFactory->createCalendar($calendar);
 
 header('Content-Type: text/calendar; charset=utf-8');
 header('Content-Disposition: attachment; filename="cal.ics"');
-echo $cal->render();
+echo $calendarComponent;
